242bcea951ba15d9906a7de4510adfd49430f88c
bp26 <aleshak260006@gmail.com>
2022-11-03 11:31:30 +0300

    feat: add functionality for appending text to file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,19 +1,38 @@
 const readline = require('node:readline');
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 const { stdin } = process;
 
-const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const filepath = path.join(__dirname, 'text.txt');
 const readlineInterface = readline.createInterface(stdin);
+
+const initFile = async (filepath) => {
+  fsPromises.access(filepath, fs.constants.F_OK).catch(() => {
+    fs.writeFile(filepath, '', (err) => {
+      if (err) throw err;
+    });
+  });
+};
+
+const appendToFile = (line) => {
+  fs.appendFile(filepath, `${line}\n`, (err) => {
+    if (err) throw err;
+  });
+};
 
 console.log('Hello. Enter text in the console:');
 
 readlineInterface.on('line', (line) => {
-  line === 'exit' ? process.exit() : output.write(`${line}\n`);
+  line === 'exit'
+    ? process.exit()
+    : initFile(filepath).then(() => appendToFile(line));
 });
+
 process.on('SIGINT', () => {
   process.exit();
 });
+
 process.on('exit', () => {
   console.log('Goodbye');
 });
