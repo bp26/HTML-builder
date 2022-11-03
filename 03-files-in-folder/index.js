de6acb15d1b472ab46bd58be72cf9090dcc8ec70
bp26 <aleshak260006@gmail.com>
2022-11-03 10:36:46 +0300

    feat: complete task 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,8 +1,46 @@
 const path = require('path');
 const fsPromises = require('fs/promises');
+const fs = require('fs');
 
-fsPromises.readdir(path.join(__dirname, './secret-folder')).then((files) => {
+const readFilesPromise = (path) => {
+  return fsPromises.readdir(path, {
+    withFileTypes: true,
+  });
+};
+
+const getStatsPromise = (filepath) => {
+  return fsPromises.stat(filepath);
+};
+
+const isFile = (stats) => {
+  return stats.isFile();
+};
+
+const getFilename = (filepath) => {
+  return path.parse(filepath).name;
+};
+
+const getExtension = (filepath) => {
+  return path.extname(filepath).split('.')[1];
+};
+
+const getSize = (stats) => {
+  const bytes = stats.size;
+  const kb = bytes / 1000;
+  return `${kb}kb`;
+};
+
+readFilesPromise(path.join(__dirname, './secret-folder')).then((files) => {
   for (const file of files) {
-    console.log(file);
+    const filepath = path.join(__dirname, 'secret-folder', file.name);
+    getStatsPromise(filepath).then((stats) => {
+      if (isFile(stats)) {
+        console.log(
+          `${getFilename(filepath)} - ${getExtension(filepath)} - ${getSize(
+            stats
+          )}`
+        );
+      }
+    });
   }
 });
