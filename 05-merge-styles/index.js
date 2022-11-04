8117808ec3831056a1202992eba9f10486e824d5
bp26 <aleshak260006@gmail.com>
2022-11-04 10:00:36 +0300

    feat: complete task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+
+const readFilesAsync = async (dir) => {
+  return fsPromises.readdir(dir, {
+    withFileTypes: true,
+  });
+};
+
+const isFile = async (filepath) => {
+  const stats = await fsPromises.stat(filepath);
+  return stats.isFile();
+};
+
+const isCss = (filepath) => {
+  const extension = path.extname(filepath);
+  return extension === '.css' ? true : false;
+};
+
+const createEmptyBundle = async (dist) => {
+  return fsPromises.writeFile(path.join(dist, 'bundle.css'), '');
+};
+
+const assembleBundleToDist = async (src, dist) => {
+  await createEmptyBundle(dist);
+  const output = fs.createWriteStream(path.join(dist, 'bundle.css'));
+  const files = await readFilesAsync(src);
+  for (const file of files) {
+    const filepath = path.join(src, file.name);
+    const isFileBoolean = await isFile(filepath);
+    const isCssBoolean = isCss(filepath);
+    if (isFileBoolean & isCssBoolean) {
+      const input = fs.createReadStream(filepath, 'utf-8');
+      input.pipe(output);
+    }
+  }
+};
+
+const src = path.join(__dirname, 'styles');
+const dist = path.join(__dirname, 'project-dist');
+
+assembleBundleToDist(src, dist);
